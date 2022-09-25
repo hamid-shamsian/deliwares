fb771c83f23842df18516179880c069f28a1ddb5
Hossein Shamsian <hamid64bits@gmail.com>
2022-09-25 13:23:43 +0330

    Initial Layouts.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layouts/Layout";
+import LandingPage from "./Components/Pages/LandingPage";
+import Shop from "./Components/Pages/Shop";
+import Blog from "./Components/Pages/Blog";
+import Contact from "./Components/Pages/Contact";
+import About from "./Components/Pages/About";
 import "./App.css";
 
 function App() {
-   return <div className='App'></div>;
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Layout />}>
+               <Route index element={<LandingPage />} />
+               <Route path='shop' element={<Shop />} />
+               <Route path='blog' element={<Blog />} />
+               <Route path='contact' element={<Contact />} />
+               <Route path='about' element={<About />} />
+            </Route>
+         </Routes>
+      </BrowserRouter>
+   );
 }
 
 export default App;
