f3b22f2e730367a1a2847ee9e231a9efb42db665
Hossein Shamsian <hamid64bits@gmail.com>
2022-10-17 13:58:01 +0330

    Pagination.

---
+++ src/Components/Common/Pagination.jsx
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import { range } from "lodash";
+
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+   const pagesCount = Math.ceil(itemsCount / pageSize);
+   if (pagesCount === 1) return null;
+   const pages = range(1, pagesCount + 1);
+   return (
+      <ul className='list pagination'>
+         <li
+            className='page__item'
+            onClick={() => currentPage - 1 && onPageChange(currentPage - 1)}
+         >
+            &lt;
+         </li>
+         {pages.map(page => (
+            <li
+               key={page}
+               className={
+                  page === currentPage ? "page__item active" : "page__item"
+               }
+               onClick={() => onPageChange(page)}
+            >
+               {page}
+            </li>
+         ))}
+         <li
+            className='page__item'
+            onClick={() =>
+               pagesCount - currentPage && onPageChange(currentPage + 1)
+            }
+         >
+            &gt;
+         </li>
+      </ul>
+   );
+};
+
+Pagination.propTypes = {
+   itemsCount: PropTypes.number.isRequired,
+   pageSize: PropTypes.number.isRequired,
+   currentPage: PropTypes.number.isRequired,
+   onPageChange: PropTypes.func.isRequired
+};
+
+export default Pagination;
--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Product from "../Blocks/Product";
+import Pagination from "../Common/Pagination";
 import getProducts from "../../services/productService";
 
 const Shop = () => {
-   const [products, setProducts] = useState(getProducts());
+   const [products, setProducts] = useState([]);
+   const [pageSize, setPageSize] = useState(0);
+   const [currentPage, setCurrentPage] = useState(1);
+
+   useEffect(() => {
+      setProducts(getProducts());
+      setPageSize(5);
+   }, []);
+
+   const handlePageChange = page => setCurrentPage(page);
+
+   const currentPageProducts = () => {
+      const startIndex = (currentPage - 1) * pageSize;
+      return products.slice(startIndex, startIndex + pageSize);
+   };
 
    return (
       <main className='block shop'>
          <h1>Shop</h1>
+         <hr />
+         <nav className='shop__nav'>
+            <Pagination
+               itemsCount={products.length}
+               pageSize={pageSize}
+               currentPage={currentPage}
+               onPageChange={handlePageChange}
+            />
+         </nav>
          <section className='flex container'>
-            {products.map(product => (
+            {currentPageProducts().map(product => (
                <Product key={product._id} data={product} />
             ))}
          </section>
